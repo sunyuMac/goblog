fbe1f56ae69b678d1a20e18b85824364599b2cc1
孙羽 <sunyu@linlaoban.com>
2020-11-23 15:36:32 +0800

    设置header

--- main.go
+++ main.go
@@ -6,8 +6,9 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	if r.URL.Path == "/" {
-		fmt.Fprint(w, "anyone")
+		fmt.Fprint(w, "welcome to goblog")
 	} else if r.URL.Path == "/sun" {
 		fmt.Fprint(w, "又来了老弟")
 	} else {
