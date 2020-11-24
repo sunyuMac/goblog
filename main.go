b933af1e53a0e24050825a28a477e002bcc5829c
孙羽 <sunyu@linlaoban.com>
2020-11-24 14:21:40 +0800

    尝试 http

--- main.go
+++ main.go
@@ -5,19 +5,41 @@ import (
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func defaultHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	if r.URL.Path == "/" {
 		fmt.Fprint(w, "welcome to goblog")
-	} else if r.URL.Path == "/sun" {
-		fmt.Fprint(w, "又来了老弟")
 	} else {
 		w.WriteHeader(http.StatusNotFound)
 		fmt.Fprint(w, "404 not found")
 	}
 }
 
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "又来了老弟")
+}
+
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":3000", nil)
+	router := http.NewServeMux()
+	router.HandleFunc("/", defaultHandler)
+	router.HandleFunc("/about", aboutHandler)
+
+	router.HandleFunc("/articles/", func(w http.ResponseWriter, r *http.Request) {
+		write := ""
+		switch r.Method {
+		case "POST":
+			write = "创建新文章"
+			break
+		case "GET":
+			write = "获取文章信息"
+			break
+		default:
+			write = "请求方式错误"
+		}
+
+		fmt.Fprint(w, write)
+	})
+
+	http.ListenAndServe(":3000", router)
 }
