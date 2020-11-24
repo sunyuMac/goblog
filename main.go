6dee74bd6ae76a29ac2519ca969ddab8d2ab217d
孙羽 <sunyu@linlaoban.com>
2020-11-24 15:42:45 +0800

    移除URL路径中的斜杠

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/gorilla/mux"
 	"net/http"
+	"strings"
 )
 
 func notFoundHandler(w http.ResponseWriter, r *http.Request) {
@@ -33,11 +34,22 @@ func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "新建文章")
 }
 
-func forceHTMLMiddleware(next http.Handler) http.Handler  {
+func forceHTMLMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// 1. 设置标头
 		w.Header().Set("Content-Type", "text/html; charset=utf-8")
 		// 2. 继续处理请求
+		next.ServeHTTP(w, r)
+	})
+}
+
+func removeTrailingSlash(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// 1. 除首页以外，移除所有请求路径后面的斜杆
+		if r.URL.Path != "/" {
+			r.URL.Path = strings.TrimSuffix(r.URL.Path, "/")
+		}
+		// 2. 将请求传递下去
 		next.ServeHTTP(w, r)
 	})
 }
@@ -58,10 +70,10 @@ func main() {
 	router.Use(forceHTMLMiddleware)
 
 	// 通过命名路由获取 URL 示例
-	homeURL,_ := router.Get("home").URL()
+	homeURL, _ := router.Get("home").URL()
 	fmt.Print("homeURL:", homeURL)
 	articleURL, _ := router.Get("articles.show").URL("id", "23")
 	fmt.Println("articleURL: ", articleURL)
 
-	http.ListenAndServe(":3000", router)
+	http.ListenAndServe(":3000", removeTrailingSlash(router))
 }
