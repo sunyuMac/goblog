1f33ff34390259a620d8ed5082c578c4c251aaa7
孙羽 <sunyu@linlaoban.com>
2020-11-25 11:13:08 +0800

    显示文章

--- main.go
+++ main.go
@@ -22,6 +22,11 @@ type ArticlesFormData struct {
 	Errors      map[string]string
 }
 
+type Article struct {
+	Title, Body string
+	ID          int64
+}
+
 var router = mux.NewRouter()
 var db *sql.DB
 
@@ -84,9 +89,27 @@ func aboutHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func articlesShowHandler(w http.ResponseWriter, r *http.Request) {
+	// 读取url中的参数
 	vars := mux.Vars(r)
 	id := vars["id"]
-	fmt.Fprint(w, "文章id："+id)
+
+	article := Article{}
+	query := "SELECT * FROM articles WHERE id = ?"
+	err := db.QueryRow(query, id).Scan(&article.ID, &article.Title, &article.Body)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			w.WriteHeader(http.StatusNotFound)
+			fmt.Fprint(w, "404 文章未找到")
+		} else {
+			checkError(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprint(w, "500 服务器内部错误")
+		}
+	} else {
+		tmpl, err := template.ParseFiles("resources/views/articles/show.gohtml")
+		checkError(err)
+		tmpl.Execute(w, article)
+	}
 }
 
 func articlesIndexHandler(w http.ResponseWriter, r *http.Request) {
