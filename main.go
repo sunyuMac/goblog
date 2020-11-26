ea377c3416f0d22d7af087ec247681d88ffb54c6
孙羽 <sunyu@linlaoban.com>
2020-11-26 13:24:43 +0800

    完成代码测试

--- main.go
+++ main.go
@@ -444,8 +444,8 @@ func main() {
 	router.HandleFunc("/articles", articlesIndexHandler).Methods("GET").Name("articles.index")
 	router.HandleFunc("/articles", articlesStoreHandler).Methods("POST").Name("articles.store")
 	router.HandleFunc("/articles/create", articlesCreateHandler).Methods("GET").Name("articles.create")
-	router.HandleFunc("/article/{id:[0-9]+}/edit", articlesEditHandler).Methods("GET").Name("articles.edit")
-	router.HandleFunc("/article/{id:[0-9]+}/update", articlesUpdateHandler).Methods("POST").Name("articles.update")
+	router.HandleFunc("/articles/{id:[0-9]+}/edit", articlesEditHandler).Methods("GET").Name("articles.edit")
+	router.HandleFunc("/articles/{id:[0-9]+}", articlesUpdateHandler).Methods("POST").Name("articles.update")
 	router.HandleFunc("/articles/{id:[0-9]+}/delete", articlesDeleteHandler).Methods("POST").Name("articles.delete")
 
 	// 自定义404页面
--- tests/pages_test.go
+++ tests/pages_test.go
@@ -2,22 +2,51 @@ package tests
 
 import (
 	"net/http"
+	"strconv"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestHomePage(t *testing.T) {
-	baseURL := "http://localhost:3000"
+func TestAllPages(t *testing.T) {
+	baseUrl := "http://localhost:3000"
 
-	// 1. 请求 —— 模拟用户访问浏览器
-	var (
-		resp *http.Response
-		err  error
-	)
-	resp, err = http.Get(baseURL + "/")
+	tests := []struct {
+		method   string
+		url      string
+		expected int
+	}{
+		{"GET", "/", 200},
+		{"GET", "/about", 200},
+		{"GET", "/notfound", 404},
+		{"GET", "/articles", 200},
+		{"GET", "/articles/create", 200},
+		{"GET", "/articles/3", 200},
+		{"GET", "/articles/3/edit", 200},
+		{"POST", "/articles/3", 200},
+		{"POST", "/articles", 200},
+		{"POST", "/articles/1/delete", 404},
+	}
 
-	// 2. 检测 —— 是否无错误且 200
-	assert.NoError(t, err, "有错误发生，err 不为空")
-	assert.Equal(t, 200, resp.StatusCode, "应返回状态码 200")
+	for _, test := range tests {
+		t.Logf("当前请求URL: %v \n", test.url)
+
+		var (
+			resp *http.Response
+			err  error
+		)
+
+		switch test.method {
+		case "POST":
+			data := make(map[string][]string)
+			resp, err = http.PostForm(baseUrl+test.url, data)
+		default:
+			resp, err = http.Get(baseUrl + test.url)
+		}
+
+		assert.NoError(t, err, "请求"+test.url+"时报错")
+		assert.Equal(t, test.expected, resp.StatusCode, test.url+"应该返回状态码"+strconv.Itoa(test.expected))
+
+	}
+
 }
