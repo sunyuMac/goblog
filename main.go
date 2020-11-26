9d6b44940da6c497e61b0da74e6550361f1fc735
孙羽 <sunyu@linlaoban.com>
2020-11-26 17:44:16 +0800

    重构 Int64ToString

--- main.go
+++ main.go
@@ -5,8 +5,9 @@ import (
 	"fmt"
 	"github.com/go-sql-driver/mysql"
 	_ "github.com/go-sql-driver/mysql"
-	"goblog/logger"
-	"goblog/route"
+	"goblog/pkg/logger"
+	"goblog/pkg/route"
+	"goblog/pkg/types"
 	"html/template"
 	"net/http"
 	"net/url"
@@ -134,7 +135,7 @@ func articlesShowHandler(w http.ResponseWriter, r *http.Request) {
 		tmpl, err := template.New("show.gohtml").
 			Funcs(template.FuncMap{
 				"RouteName2URL": route.Name2URL,
-				"Int64ToString": Int64ToString,
+				"Int64ToString": types.Int64ToString,
 			}).
 			ParseFiles("resources/views/articles/show.gohtml")
 		logger.LogError(err)
@@ -349,11 +350,6 @@ func articlesUpdateHandler(w http.ResponseWriter, r *http.Request) {
 			tmpl.Execute(w, data)
 		}
 	}
-}
-
-// Int64ToString 将 int64 转换为 string
-func Int64ToString(num int64) string {
-	return strconv.FormatInt(num, 10)
 }
 
 func (a Article) Delete() (int64, error) {
