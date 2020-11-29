ce8295822511250aec707e558f0faa5fe45d6397
孙羽 <sunyu@linlaoban.com>
2020-11-29 22:19:09 +0800

    重构更新文章

--- app/models/article/crud.go
+++ app/models/article/crud.go
@@ -2,8 +2,8 @@ package article
 
 import (
 	"goblog/pkg/logger"
-	"goblog/pkg/types"
 	"goblog/pkg/model"
+	"goblog/pkg/types"
 )
 
 func Get(idstr string) (Article, error) {
@@ -15,7 +15,7 @@ func Get(idstr string) (Article, error) {
 	return article, nil
 }
 
-func GetAll() ([]Article, error)  {
+func GetAll() ([]Article, error) {
 	var articles []Article
 	if err := model.DB.Find(&articles).Error; err != nil {
 		return articles, err
@@ -24,10 +24,20 @@ func GetAll() ([]Article, error)  {
 }
 
 func (article *Article) Create() (err error) {
-	if err =  model.DB.Create(&article).Error; err != nil {
+	if err = model.DB.Create(&article).Error; err != nil {
 		logger.LogError(err)
 		return err
 	}
 
 	return nil
+}
+
+func (article *Article) Update() (rowsAffected int64, err error) {
+	result := model.DB.Save(&article)
+	if err = result.Error; err != nil {
+		logger.LogError(err)
+		return 0, err
+	}
+
+	return result.RowsAffected, nil
 }
--- main.go
+++ main.go
@@ -14,7 +14,6 @@ import (
 	"net/url"
 	"strconv"
 	"strings"
-	"unicode/utf8"
 )
 
 type Article struct {
@@ -90,73 +89,6 @@ type ArticlesFormData struct {
 	URL         *url.URL
 	Errors      map[string]string
 }
-func validateArticleFormData(title, body string) map[string]string {
-	errors := make(map[string]string)
-
-	if title == "" {
-		errors["title"] = "标题不能为空"
-	} else if utf8.RuneCountInString(title) < 3 || utf8.RuneCountInString(title) > 40 {
-		errors["title"] = "标题长度需介于3~40"
-	}
-
-	if body == "" {
-		errors["body"] = "内容不能为空"
-	} else if utf8.RuneCountInString(body) < 10 {
-		errors["body"] = "内容长度需要大于10个字符"
-	}
-	return errors
-}
-func articlesUpdateHandler(w http.ResponseWriter, r *http.Request) {
-	// 获取url中的id
-	id := getRouteVariable("id", r)
-
-	_, err := getArticleByID(id)
-	if err != nil {
-		if err == sql.ErrNoRows {
-			w.WriteHeader(http.StatusNotFound)
-			fmt.Fprint(w, "404 文章不存在")
-		} else {
-			w.WriteHeader(http.StatusInternalServerError)
-			fmt.Fprint(w, "500 服务器内部错误")
-		}
-	} else {
-		title := r.PostFormValue("title")
-		body := r.PostFormValue("body")
-
-		errors := validateArticleFormData(title, body)
-		if len(errors) == 0 {
-			rs, err := articlesUpdateToDb(id, title, body)
-			if err != nil {
-				// 数据库错误
-				logger.LogError(err)
-				w.WriteHeader(http.StatusInternalServerError)
-				fmt.Fprint(w, "500 服务器内部错误")
-			}
-
-			if n, _ := rs.RowsAffected(); n > 0 {
-				showUrl, _ := router.Get("articles.show").URL("id", id)
-				http.Redirect(w, r, showUrl.String(), http.StatusFound)
-			} else {
-				fmt.Fprint(w, "您没有做任何修改")
-			}
-		} else {
-			updateUrl, _ := router.Get("articles.update").URL("id", id)
-			data := ArticlesFormData{
-				Title:  title,
-				Body:   body,
-				URL:    updateUrl,
-				Errors: errors,
-			}
-
-			tmpl, err := template.ParseFiles("resources/views/articles/edit.gohtml")
-			if err != nil {
-				panic(err)
-			}
-
-			tmpl.Execute(w, data)
-		}
-	}
-}
 
 func (a Article) Delete() (int64, error) {
 	rs, err := db.Exec("DELETE FROM articles WHERE id = " + strconv.FormatInt(a.ID, 10))
@@ -213,12 +145,6 @@ func getRouteVariable(parameterName string, r *http.Request) string {
 	return mux.Vars(r)[parameterName]
 }
 
-func articlesUpdateToDb(id, title, body string) (rs sql.Result, err error) {
-	query := "UPDATE articles SET title = ?, body = ? WHERE id = ?"
-	rs, err = db.Exec(query, title, body, id)
-	return
-}
-
 func main() {
 	database.Initialize()
 	db = database.DB
@@ -227,8 +153,7 @@ func main() {
 	router = bootstrap.SetupRoute()
 	route.SetRoute(router)
 
-	router.HandleFunc("/articles/{id:[0-9]+}/edit", articlesEditHandler).Methods("GET").Name("articles.edit")
-	router.HandleFunc("/articles/{id:[0-9]+}", articlesUpdateHandler).Methods("POST").Name("articles.update")
+
 	router.HandleFunc("/articles/{id:[0-9]+}/delete", articlesDeleteHandler).Methods("POST").Name("articles.delete")
 
 	// 中间件：强制header内容类型为 HTML
